a07b7626ee1d8f86cd7370c781be02d13d391205
Grecu Iulia <grecuiulia11@gmail.com>
2026-06-28 12:40:43 +0300

    Commit

---
+++ src/main/java/com/filsoft/filsoftcreditsolutionproject/FilsoftCreditSolutionProjectApplication.java
@@ -0,0 +1,14 @@
+package com.filsoft.filsoftcreditsolutionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilsoftCreditSolutionProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Salut");
+        SpringApplication.run(FilsoftCreditSolutionProjectApplication.class, args);
+    }
+
+}
